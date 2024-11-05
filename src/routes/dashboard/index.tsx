6095556595e9eafa298002dcf16ff9fe1224aa87
Zaya1101 <isaiah_landicho@hotmail.com>
2024-11-05 13:33:23 +1000

    implemented dashboard layout

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,10 +1,35 @@
 import * as React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
 
+import Card from 'components/Card';
+
+import "./dashboard.css";
+
 export const Route = createFileRoute('/dashboard/')({
   component: Dashboard,
 })
 
 function Dashboard() {
-  return 'Hello /dashboard/!'
+  return (
+    <div id="Dashboard">
+      <div className="dashboard-grid">
+        <div className="dashboard-grid-col-1">
+          <Card title="Recent Comments" className="recent-comments-widget">
+            <p>There are no comments yet.</p>
+          </Card>
+        </div>
+        <div className="dashboard-grid-col-2">
+          <Card title="Likes/Dislikes" className="likes-dislikes-widget">
+            <p>Likes/Dislikes Chart</p>
+          </Card>
+          <Card title="Audience Breakdown" className="audience-widget">
+            <p>Audience Breakdown Chart</p>
+          </Card>
+          <Card title="Total Views" className="total-views-widget">
+            <p>Total Views Chart</p>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 }
