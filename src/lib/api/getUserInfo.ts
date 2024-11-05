154eda5b7d0328afb9e5c16760e80cecaaba339f
Zaya1101 <isaiah_landicho@hotmail.com>
2024-11-05 23:08:51 +1000

    removed signal from getUserInfo

--- src/lib/api/getUserInfo.ts
+++ src/lib/api/getUserInfo.ts
@@ -6,12 +6,11 @@ type UserInfo = {
   error_description?: string;
 }
 
-export default async function getUserInfo(accessToken: string, signal: AbortSignal) {
+export default async function getUserInfo(accessToken: string) {
   const userInfo: UserInfo = await fetch("https://www.googleapis.com/oauth2/v3/userinfo", {
     headers: {
       Authorization: `Bearer ${accessToken}`
     },
-    signal: signal
   })
     .then((response) => response.json())
     .catch((error) => {
