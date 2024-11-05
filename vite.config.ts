1e553c1122c4c303fd18e8906b8f7e4c9b4708e4
Zaya1101 <isaiah_landicho@hotmail.com>
2024-11-05 12:45:10 +1000

    absolute imports config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
+import tsConfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({}), 
+    tsConfigPaths(),
     react()
   ],
+  resolve: {
+    alias: {
+      src: "/src",
+    },
+  },
 })
