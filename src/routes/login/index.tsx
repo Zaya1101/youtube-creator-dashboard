e4c77fbcf9ea94db70ab01024265ca30acbfd75b
Zaya1101 <isaiah_landicho@hotmail.com>
2024-11-05 13:01:14 +1000

    basic login design

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -1,10 +1,27 @@
 import * as React from 'react'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
+
+import Card from 'components/Card'
+import Button from 'components/Button';
+
+import "./login.css";
 
 export const Route = createFileRoute('/login/')({
   component: Login,
 })
 
 function Login() {
-  return 'Hello /login/!'
+  return (
+    <div id="Login" className="login">
+      <Card title="YouTube Creator Dashboard" titleAlignment="center">
+        <div className="login-card-body">
+          <Link to="/dashboard">
+            <Button>
+              Sign In With Google
+            </Button>
+          </Link>
+        </div>
+      </Card>
+    </div>
+  );
 }
